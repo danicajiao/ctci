7e209950d3c122ca6935eb7369841b35340b41b5
Daniel Cajiao <cajiao.dani@gmail.com>
2021-08-22 16:26:35 -0400

    Initial commit

---
+++ java/binary-search/BinarySearch.java
@@ -0,0 +1,18 @@
+/**
+ * BinarySearch
+ */
+public class BinarySearch
+{
+    public static void main(String[] args)
+    {
+        int[] nums = { 1, 5, 8, 9, 11, 13, 15, 19, 21 };
+        int low = 0;
+        int high = nums.length - 1;
+
+        binarySearch(nums, low, high, 19);
+    }
+
+    public static void binarySearch(int[] nums, int low, int high, int x)
+    {
+    }
+}
